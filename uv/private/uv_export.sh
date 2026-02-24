d3fb29210019f787ad05df7e3bddcfdcbd70fd9b
nikonikolov <niko.b.nikolov@gmail.com>
2026-02-24 23:19:36 +0000

    reset rule

--- uv/private/uv_export.sh
+++ uv/private/uv_export.sh
@@ -7,23 +7,8 @@ PYPROJECT_TOML="{{pyproject_toml}}"
 REQUIREMENTS_TXT="{{requirements_txt}}"
 COMPILE_COMMAND="{{compile_command}}"
 
-# PYPROJECT_TOML="$(realpath "$PYPROJECT_TOML")"
-# REQUIREMENTS_TXT="$(realpath "$REQUIREMENTS_TXT")"
-if [ -n "${BUILD_WORKSPACE_DIRECTORY:-}" ]; then
-    PYPROJECT_TOML="$BUILD_WORKSPACE_DIRECTORY/$PYPROJECT_TOML"
-    REQUIREMENTS_TXT="$BUILD_WORKSPACE_DIRECTORY/$REQUIREMENTS_TXT"
-else
-    PYPROJECT_TOML="$(realpath "$PYPROJECT_TOML")"
-    REQUIREMENTS_TXT="$(realpath "$REQUIREMENTS_TXT")"
-fi
-
-updated_file=$(mktemp -p "$(dirname "$REQUIREMENTS_TXT")" ".$(basename "$REQUIREMENTS_TXT").tmp.XXXXXX")
-trap 'rm -f "$updated_file"' EXIT
-
 {{uv}} export \
     {{args}} \
     --project="$(dirname "$PYPROJECT_TOML")" \
-    --output-file="$updated_file" \
+    --output-file="$REQUIREMENTS_TXT" \
     "$@"
-
-cp -f "$updated_file" "$REQUIREMENTS_TXT"
