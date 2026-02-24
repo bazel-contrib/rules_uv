ab72e61e6f7253dbb08af81179e21cd6d30ee8b6
nikonikolov <niko.b.nikolov@gmail.com>
2026-02-24 22:29:20 +0000

    maybe enable repeated compiles

--- uv/private/uv_export.sh
+++ uv/private/uv_export.sh
@@ -7,13 +7,16 @@ PYPROJECT_TOML="{{pyproject_toml}}"
 REQUIREMENTS_TXT="{{requirements_txt}}"
 COMPILE_COMMAND="{{compile_command}}"
 
+PYPROJECT_TOML="$(realpath "$PYPROJECT_TOML")"
+REQUIREMENTS_TXT="$(realpath "$REQUIREMENTS_TXT")"
+
 updated_file=$(mktemp -p "$(dirname "$REQUIREMENTS_TXT")" ".$(basename "$REQUIREMENTS_TXT").tmp.XXXXXX")
 trap 'rm -f "$updated_file"' EXIT
 
 {{uv}} export \
     {{args}} \
     --project="$(dirname "$PYPROJECT_TOML")" \
-    --output-file="$updated_file" \
-    "$@"
+    "$@" \
+    > "$updated_file"
 
 cp -f "$updated_file" "$REQUIREMENTS_TXT"
